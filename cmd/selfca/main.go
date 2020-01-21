78c7b1a04be418dd5c1502f5d0f4463190bf0014
Li Kexian <i@likexian.com>
2020-01-21 23:00:45 +0800

    split to module and command

---
+++ cmd/selfca/main.go
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2014-2020 Li Kexian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Go module for Self-Signed Certificate Generating
+ * https://www.likexian.com/
+ */
+
+package main
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/likexian/selfca"
+)
+
+func main() {
+	host := flag.String("h", "", "Domains or IPs of the certificate, comma separated")
+	start := flag.String("s", "", "Valid from of the certificate, formatted as 2006-01-02 15:04:05 (default now)")
+	days := flag.Int("d", 365, "Valid days of the certificate, for example 365 (default 365 days)")
+	output := flag.String("o", "cert", "Folder for saving the certificate (default cert)")
+	flag.Parse()
+
+	var hosts []string
+	for _, v := range strings.Split(*host, ",") {
+		v = strings.TrimSpace(v)
+		if v != "" {
+			hosts = append(hosts, v)
+		}
+	}
+
+	if len(hosts) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var notBefore time.Time
+	if len(*start) == 0 {
+		notBefore = time.Now()
+	} else {
+		var err error
+		notBefore, err = time.Parse("2006-01-02 15:04:05", *start)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse valid from parameter: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	notAfter := notBefore.Add(time.Duration(*days) * 24 * time.Hour)
+
+	if len(*output) == 0 {
+		*output = "cert"
+	}
+
+	if _, err := os.Stat(*output); os.IsNotExist(err) {
+		err = os.MkdirAll(*output, 0755)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to create output folder: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	var caCertificate []*x509.Certificate
+	var caKey *rsa.PrivateKey
+	var certificate []byte
+	var key *rsa.PrivateKey
+	var err error
+
+	caPath := fmt.Sprintf("%s/ca", *output)
+	if _, err := os.Stat(caPath + ".crt"); err == nil {
+		caCertificate, caKey, err = selfca.ReadCertificate(caPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to load ca certificate: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		caNotAfter := notBefore.Add(10 * 365 * 24 * time.Hour)
+		certificate, caKey, err = selfca.GenerateCertificate(selfca.Certificate{
+			IsCA:      true,
+			NotBefore: notBefore,
+			NotAfter:  caNotAfter,
+		})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to generate ca certificate: %v\n", err)
+			os.Exit(1)
+		}
+
+		err = selfca.WriteCertificate(caPath, certificate, caKey)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to write ca certificate: %v\n", err)
+			os.Exit(1)
+		}
+
+		caCertificate, err = x509.ParseCertificates(certificate)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse ca certificate: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	certificate, key, err = selfca.GenerateCertificate(selfca.Certificate{
+		IsCA:          false,
+		NotBefore:     notBefore,
+		NotAfter:      notAfter,
+		Hosts:         hosts,
+		CAKey:         caKey,
+		CACertificate: caCertificate[0],
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate the certificate: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = selfca.WriteCertificate(fmt.Sprintf("%s/%s", *output, hosts[0]), certificate, key)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to write the certificate: %v\n", err)
+		os.Exit(1)
+	}
+}
--- selfca.go
+++ selfca.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2019 Li Kexian
+ * Copyright 2014-2020 Li Kexian
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
  * https://www.likexian.com/
  */
 
-package main
+package selfca
 
 import (
 	"crypto/rand"
@@ -25,128 +25,22 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"math/big"
 	"net"
 	"os"
-	"strings"
 	"time"
 )
 
 // Certificate stors certificate information for generating
 type Certificate struct {
-	isCa          bool
-	hosts         []string
-	caKey         *rsa.PrivateKey
-	caCertificate *x509.Certificate
-	notBefore     time.Time
-	notAfter      time.Time
-}
-
-func main() {
-	host := flag.String("h", "", "Domains or IPs of the certificate, comma separated")
-	start := flag.String("s", "", "Valid from of the certificate, formatted as 2006-01-02 15:04:05 (default now)")
-	days := flag.Int("d", 365, "Valid days of the certificate, for example 365 (default 365 days)")
-	output := flag.String("o", "cert", "Folder for saving the certificate (default cert)")
-	flag.Parse()
-
-	var hosts []string
-	for _, v := range strings.Split(*host, ",") {
-		v = strings.TrimSpace(v)
-		if v != "" {
-			hosts = append(hosts, v)
-		}
-	}
-
-	if len(hosts) == 0 {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	var notBefore time.Time
-	if len(*start) == 0 {
-		notBefore = time.Now()
-	} else {
-		var err error
-		notBefore, err = time.Parse("2006-01-02 15:04:05", *start)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to parse valid from parameter: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	notAfter := notBefore.Add(time.Duration(*days) * 24 * time.Hour)
-
-	if len(*output) == 0 {
-		*output = "cert"
-	}
-
-	if _, err := os.Stat(*output); os.IsNotExist(err) {
-		err = os.MkdirAll(*output, 0755)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to create output folder: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	var caCertificate []*x509.Certificate
-	var caKey *rsa.PrivateKey
-	var certificate []byte
-	var key *rsa.PrivateKey
-	var err error
-
-	caPath := fmt.Sprintf("%s/ca", *output)
-	if _, err := os.Stat(caPath + ".crt"); err == nil {
-		caCertificate, caKey, err = ReadCertificate(caPath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to load ca certificate: %v\n", err)
-			os.Exit(1)
-		}
-	} else {
-		caNotAfter := notBefore.Add(10 * 365 * 24 * time.Hour)
-		certificate, caKey, err = GenerateCertificate(Certificate{
-			isCa:      true,
-			notBefore: notBefore,
-			notAfter:  caNotAfter,
-		})
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to generate ca certificate: %v\n", err)
-			os.Exit(1)
-		}
-
-		err = WriteCertificate(caPath, certificate, caKey)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to write ca certificate: %v\n", err)
-			os.Exit(1)
-		}
-
-		caCertificate, err = x509.ParseCertificates(certificate)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to parse ca certificate: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	certificate, key, err = GenerateCertificate(Certificate{
-		isCa:          false,
-		hosts:         hosts,
-		caKey:         caKey,
-		caCertificate: caCertificate[0],
-		notBefore:     notBefore,
-		notAfter:      notAfter,
-	})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to generate the certificate: %v\n", err)
-		os.Exit(1)
-	}
-
-	err = WriteCertificate(fmt.Sprintf("%s/%s", *output, hosts[0]), certificate, key)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to write the certificate: %v\n", err)
-		os.Exit(1)
-	}
+	IsCA          bool
+	NotBefore     time.Time
+	NotAfter      time.Time
+	Hosts         []string
+	CAKey         *rsa.PrivateKey
+	CACertificate *x509.Certificate
 }
 
 // GenerateCertificate generates X.509 certificate and key
@@ -165,25 +59,25 @@ func GenerateCertificate(c Certificate) ([]byte, *rsa.PrivateKey, error) {
 	template := x509.Certificate{
 		SerialNumber:          serialNumber,
 		Subject:               pkix.Name{},
-		NotBefore:             c.notBefore,
-		NotAfter:              c.notAfter,
-		IsCA:                  c.isCa,
+		NotBefore:             c.NotBefore,
+		NotAfter:              c.NotAfter,
+		IsCA:                  c.IsCA,
 		BasicConstraintsValid: true,
 	}
 
-	if c.isCa {
+	if c.IsCA {
 		template.Subject.CommonName = "Root CA"
 		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign
 		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth}
-		c.caKey = key
-		c.caCertificate = &template
+		c.CAKey = key
+		c.CACertificate = &template
 	} else {
-		template.Subject.CommonName = c.hosts[0]
+		template.Subject.CommonName = c.Hosts[0]
 		template.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment
 		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth}
 	}
 
-	for _, v := range c.hosts {
+	for _, v := range c.Hosts {
 		if ip := net.ParseIP(v); ip != nil {
 			template.IPAddresses = append(template.IPAddresses, ip)
 		} else {
@@ -191,7 +85,7 @@ func GenerateCertificate(c Certificate) ([]byte, *rsa.PrivateKey, error) {
 		}
 	}
 
-	certificate, err := x509.CreateCertificate(rand.Reader, &template, c.caCertificate, &key.PublicKey, c.caKey)
+	certificate, err := x509.CreateCertificate(rand.Reader, &template, c.CACertificate, &key.PublicKey, c.CAKey)
 	if err != nil {
 		return nil, nil, err
 	}
